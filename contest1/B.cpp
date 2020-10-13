50dda46dff8e480e6026f501f774cdc8a216f5c4
ris-iris <sagitova.ma@phystech.edu>
2020-10-13 15:50:35 +0300

    fix

--- contest1/B.cpp
+++ contest1/B.cpp
@@ -1,53 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 
-uint64_t odd(const std::string& s){
+template <int is_even>
+uint64_t count_polindroms(const std::string& s){
   int n = s.length();
-  std::vector<int> odd(n, 0);
+  std::vector<int> polindroms(n, 0);
   uint64_t ans = 0;
-  int l = 0;
-  int r = -1;
+  int left = 0;
+  int right = -1;
   for (int i = 0; i < n; ++i){
-    if(i <= r)
-      odd[i] = std::min(r - i + 1, odd[l+r-i]);
-    while (i + odd[i] < n && i - odd[i] > -1 && s[i - odd[i]] == s[odd[i] + i])
-      ++odd[i];
-    if(i + odd[i] - 1 > r){
-      l = i - odd[i] + 1;
-      r = i + odd[i] - 1;
+    if(i <= right)
+      polindroms[i] = std::min(right - i + 1, polindroms[left+right-i+is_even]);
+    while (i + polindroms[i] < n && i - polindroms[i] - is_even > -1 && s[polindroms[i] + i] == s[i - polindroms[i] - is_even])
+      ++polindroms[i];
+    if(i + polindroms[i] - 1 > right){
+      left = i - polindroms[i] + (1 - is_even);
+      right = i + polindroms[i] - 1;
     }
   }
-  for(auto v = odd.begin(); v != odd.end(); ++v){
-    ans += *v;
-  }
-  return ans;
-}
-
-uint64_t even(const std::string& s){
-  int n = s.length();
-  std::vector<int> even(n, 0);
-  uint64_t ans = 0;
-  int l = 0;
-  int r = -1;
-  for (int i = 0; i < n; ++i){
-    if(i <= r)
-      even[i] = std::min(r - i + 1, even[l+r-i+1]);
-    while (i + even[i] < n && i - even[i] -1 > -1 && s[even[i] + i] == s[i - even[i] - 1])
-      ++even[i];
-    if(i + even[i] - 1 > r){
-      l = i - even[i];
-      r = i + even[i] - 1;
-    }
-  }
-  for(auto v = even.begin(); v != even.end(); ++v){
-    ans += *v;
-  }
+  ans = std::accumulate(polindroms.begin(), polindroms.end(), (uint64_t)0);
   return ans;
 }
 
 int main(){
   std::string s;
   std::getline(std::cin, s);
-  std::cout << even(s) + odd(s) - s.length();
+  std::cout << count_polindroms<0>(s) + count_polindroms<1>(s) - s.length();
   return 0;
 }
