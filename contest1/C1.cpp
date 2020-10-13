da717cb6e7b1117798f56963f153a5aebec71c58
ris-iris <sagitova.ma@phystech.edu>
2020-10-13 23:43:43 +0300

    fix

--- contest1/C1.cpp
+++ contest1/C1.cpp
@@ -1,79 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
-#define ALPHABET 26
-
-class Bor {
- public:
-  Bor(const std::string &m): mask(m) {
-    root = new Node(nullptr, 0);
-    root->parent = root;
-    root->sufLink = root;
-    findSubpatterns();
-    for (int i = 0; i < subpatternPos.size(); ++i) {
-      addString(subpatternPos[i], i);
-    }
-  }
-
-  std::vector<int> findPatterns(const std::string &text) {
-    std::vector<int> maskPos(text.length(), 0);
-    Node* curNode = root;
-    Node* nextTerminal;
-    std::vector<int> ans;
-
-    if (subpatternPos.empty()){
-      for (int i = 0; i < text.length() - mask.length() + 1; ++i) {
-          ans.push_back(i);
-      }
-      return ans;
-    }
-
-    for (int i = 0; i < text.length(); ++i) {
-      curNode = getGo(curNode, text[i]);
-      nextTerminal = curNode;
-      do {
-        if (nextTerminal->isTerminal){
-          for (auto num = nextTerminal->patternIndex.begin(); num != nextTerminal->patternIndex.end(); ++num){
-            int startInd = i - subpatternPos[*num].second + 1;
-            if(startInd > -1 && startInd + mask.length() - 1 < text.length()) {
-              ++maskPos[startInd];
-            }
-          }
-        }
-        nextTerminal = getSufLink(nextTerminal);
-      } while (nextTerminal != root);
-    }
-
-    for (int i = 0; i < text.length(); ++i) {
-      if (maskPos[i] == subpatternPos.size()){
-        ans.push_back(i);
-      }
-    }
-    return ans;
-  }
-
- private:
+template<int ALPHABET, char FIRST_CHAR>
+class Trie {
   struct Node {
     Node *children[ALPHABET];
-    Node *go[ALPHABET];
-    Node *sufLink;
-    Node *parent;
     char value;
-    bool isTerminal;
-    std::vector<int> patternIndex;
+    bool is_terminal;
+    std::vector<int> str_id;
+    Node *parent;
+    Node *suf_link;
+    Node *short_suf_link;
+    Node *go[ALPHABET];
 
-    Node(Node *p, char c) :
-        parent(p),
-        value(c),
-        sufLink(nullptr),
-        isTerminal(false) {
+    Node(char v, bool b, Node *p) {
+      value = v;
+      is_terminal = b;
+      parent = p;
+      suf_link = nullptr;
+      short_suf_link = nullptr;
       for (int i = 0; i < ALPHABET; ++i) {
         children[i] = nullptr;
         go[i] = nullptr;
       }
     }
-
-    ~Node(){
+    ~Node() {
       for (int i = 0; i < ALPHABET; ++i) {
         if (children[i] != nullptr)
           delete children[i];
@@ -81,71 +33,187 @@ class Bor {
     }
   };
 
-  void addString(const std::pair<int, int> &pat, int patNum) {
-    Node* curNode = root;
-    for (int i = pat.first; i < pat.second; ++i) {
-      char curChar = mask[i];
-      if (curNode->children[curChar - 'a'] == nullptr){
-        Node* newNode = new Node(curNode, curChar);
-        curNode->children[curChar - 'a'] = newNode;
+  Node *root_;
+
+  void AddString(const std::string &s, int num) {
+    Node *cur = root_;
+    for (int i = 0; i < s.length(); ++i) {
+      char curChar = s[i];
+      if (cur->children[curChar - FIRST_CHAR] == nullptr) {
+        cur->children[curChar - FIRST_CHAR] = new Node(curChar, false, cur);
       }
-      curNode = curNode->children[curChar - 'a'];
+      cur = cur->children[curChar - FIRST_CHAR];
     }
-    curNode->isTerminal = true;
-    curNode->patternIndex.push_back(patNum);
+    cur->is_terminal = true;
+    cur->str_id.push_back(num);
   }
 
-  Node *getSufLink(Node *n) {
-    if (n->sufLink == nullptr) {
-      if (n->parent == root) n->sufLink = root;
-      else n->sufLink = getGo(getSufLink(n->parent), n->value);
+  Node *GetSufLink(Node *n) {
+    if (n->suf_link == nullptr) {
+      if (n == root_ || n->parent == root_) {
+        n->suf_link = root_;
+      } else {
+        n->suf_link = GetGo(GetSufLink(n->parent), n->value);
+      }
     }
-    return n->sufLink;
+    return n->suf_link;
   }
 
-  Node *getGo(Node *n, char c) {
-    if (n->go[c - 'a'] == nullptr) {
-      if (n->children[c - 'a'] != nullptr)
-        n->go[c - 'a'] = n->children[c - 'a'];
-      else if (n == root)
-        n->go[c - 'a'] = root;
-      else
-        n->go[c - 'a'] = getGo(getSufLink(n), c);
+  Node *GetGo(Node *n, char c) {
+    if (n->go[c - FIRST_CHAR] == nullptr) {
+      if (n->children[c - FIRST_CHAR] != nullptr) {
+        n->go[c - FIRST_CHAR] = n->children[c - FIRST_CHAR];
+      } else if (n == root_) {
+        n->go[c - FIRST_CHAR] = root_;
+      } else {
+        n->go[c - FIRST_CHAR] = GetGo(GetSufLink(n), c);
+      }
     }
-    return n->go[c - 'a'];
+    return n->go[c - FIRST_CHAR];
   }
 
-  void findSubpatterns() { //CHECK
-    std::pair<int, int> curSubPos;
-    int m = mask.length();
-    for (int i = 0; i < m; ++i) {
-      while (i < m && mask[i] == '?')
-        ++i;
-      if(i == m)
-        break;
-      curSubPos.first = i;
-      while (i < m && mask[i] != '?')
-        ++i;
+  Node *GetShortSufLink(Node *n) {
+    if (n->short_suf_link == nullptr) {
+      Node *sufLink = GetSufLink(n);
+      if (sufLink->is_terminal) {
+        n->short_suf_link = sufLink;
+      } else if (sufLink == root_) {
+        n->short_suf_link = root_;
+      } else {
+        n->short_suf_link = GetShortSufLink(sufLink);
+      }
+    }
+    return n->short_suf_link;
+  }
 
-      curSubPos.second = i;
-      subpatternPos.push_back(curSubPos);
+ public:
+  Trie(std::vector<std::string> patterns) {
+    root_ = new Node(0, false, nullptr);
+    for (int i = 0; i < patterns.size(); ++i) {
+      AddString(patterns[i], i);
     }
   }
 
-  std::string mask;
-  Node *root;
-  std::vector<std::pair<int, int> > subpatternPos;
+  ~Trie() {
+    delete root_;
+  }
+
+  template<int A, char F>
+  friend
+  class Iter;
 };
 
-int main() {
-  std::string pattern, text;
-  std::cin >> pattern >> text;
+template<int ALPHABET, char FIRST_CHAR>
+class Iter {
+  Trie<ALPHABET, FIRST_CHAR>* temp_trie_;
+  typename Trie<ALPHABET, FIRST_CHAR>::Node *temp_node_;
+ public:
+  Iter(Iter const &iter) {
+    temp_trie_ = iter.temp_trie_;
+    temp_node_ = iter.temp_node_;
+  }
 
-  Bor ahoCorasickMachine(pattern);
+  Iter(Trie<ALPHABET, FIRST_CHAR>* t) {
+    temp_trie_ = t;
+    temp_node_ = t->root_;
+  }
 
-  std::vector<int> entries = ahoCorasickMachine.findPatterns(text);
-  for (int i = 0; i < entries.size(); i++) {
-    std::cout << entries[i] << " ";
+  void Go(char c) {
+    temp_node_ = temp_trie_->GetGo(temp_node_, c);
+  }
+
+  bool IsTerminal() {
+    return temp_node_->is_terminal;
+  }
+
+  auto PatternsBegin() {
+    return temp_node_->str_id.begin();
+  }
+
+  auto PatternsEnd() {
+    return temp_node_->str_id.end();
+  }
+
+  void Next() {
+    temp_node_ = temp_trie_->GetShortSufLink(temp_node_);
+  }
+
+  bool IsFinal() {
+    return temp_node_ == temp_trie_->root_;
+  }
+
+  ~Iter() {}
+
+};
+
+template<int ALPHABET, char FIRST_CHAR>
+void FindPatterns(Trie<ALPHABET, FIRST_CHAR> *trie,
+                  std::vector<int> &c,
+                  const std::vector<int> &pattern_pos,
+                  const std::string &text) {
+  Iter<ALPHABET, FIRST_CHAR> cur = Iter<ALPHABET, FIRST_CHAR>(trie);
+  Iter<ALPHABET, FIRST_CHAR> next_terminal = Iter<ALPHABET, FIRST_CHAR>(cur);
+  for (int i = 0; i < text.length(); ++i) {
+    cur.Go(text[i]);
+    next_terminal = cur;
+    do {
+      if (next_terminal.IsTerminal()) {
+        for (auto num = next_terminal.PatternsBegin(); num != next_terminal.PatternsEnd(); ++num) {
+          int startInd = i - pattern_pos[*num];
+          if (startInd > -1 && startInd + pattern_pos[pattern_pos.size() - 1] <= text.length()) {
+            ++c[startInd];
+          }
+        }
+      }
+      next_terminal.Next();
+    } while (!next_terminal.IsFinal());
+  }
+}
+
+void Separate(std::string s, std::vector<std::string> &ans, std::vector<int> &ansPos) {
+  s = "?" + s;
+  int n = -1;
+  for (int i = 0; i < s.length(); ++i) {
+    while (s[i] == '?') {
+      ++i;
+    }
+
+    if (i == s.length())
+      break;
+
+    if (s[i - 1] == '?') {
+      ans.push_back("");
+      ++n;
+    }
+    ans[n] += s[i];
+    if (i == s.length() - 1 || s[i + 1] == '?')
+      ansPos.push_back(i - 1);
+  }
+  ansPos.push_back(s.length() - 1);
+}
+
+int main(int argc, char *argv[]) {
+  std::string pat, text;
+  std::getline(std::cin, pat);
+  std::getline(std::cin, text);
+
+  std::vector<std::string> patterns;
+  std::vector<int> pattern_positions;
+  Separate(pat, patterns, pattern_positions);
+  int patterns_number = patterns.size();
+
+  Trie<26, 'a'> b(patterns);
+  std::vector<int> c(text.length(), 0);
+  FindPatterns(&b, c, pattern_positions, text);
+
+  if (patterns_number == 0) {
+    for (int i = 0; i < text.length() - pat.size() + 1; ++i) {
+      std::cout << i << " ";
+    }
+  } else {
+    for (int i = 0; i < text.length(); ++i) {
+      if (c[i] == patterns_number) std::cout << i << " ";
+    }
   }
 
   return 0;
