cb499abed5664cf80565466bc45423067a245cb9
ris-iris <sagitova.ma@phystech.edu>
2020-10-19 07:46:41 +0300

    fix

--- contest1/B.cpp
+++ contest1/B.cpp
@@ -2,30 +2,31 @@
 #include <vector>
 #include <numeric>
 
-template <int is_even>
-uint64_t count_polindroms(const std::string& s){
+template<int is_even>
+uint64_t CountPalindromes(const std::string &s) {
   int n = s.length();
-  std::vector<int> polindroms(n, 0);
+  std::vector<int> palindromes(n, 0);
   uint64_t ans = 0;
   int left = 0;
   int right = -1;
-  for (int i = 0; i < n; ++i){
-    if(i <= right)
-      polindroms[i] = std::min(right - i + 1, polindroms[left+right-i+is_even]);
-    while (i + polindroms[i] < n && i - polindroms[i] - is_even > -1 && s[polindroms[i] + i] == s[i - polindroms[i] - is_even])
-      ++polindroms[i];
-    if(i + polindroms[i] - 1 > right){
-      left = i - polindroms[i] + (1 - is_even);
-      right = i + polindroms[i] - 1;
+  for (int i = 0; i < n; ++i) {
+    if (i <= right)
+      palindromes[i] = std::min(right - i + 1, palindromes[left + right - i + is_even]);
+    while (i + palindromes[i] < n && i - palindromes[i] - is_even > -1
+        && s[palindromes[i] + i] == s[i - palindromes[i] - is_even])
+      ++palindromes[i];
+    if (i + palindromes[i] - 1 > right) {
+      left = i - palindromes[i] + (1 - is_even);
+      right = i + palindromes[i] - 1;
     }
   }
-  ans = std::accumulate(polindroms.begin(), polindroms.end(), (uint64_t)0);
+  ans = std::accumulate(palindromes.begin(), palindromes.end(), (uint64_t) 0);
   return ans;
 }
 
-int main(){
+int main() {
   std::string s;
   std::getline(std::cin, s);
-  std::cout << count_polindroms<0>(s) + count_polindroms<1>(s) - s.length();
+  std::cout << CountPalindromes<0>(s) + CountPalindromes<1>(s) - s.length();
   return 0;
 }
