388dca8af4383f062f36da710e45ee5f70c1e33f
ris-iris <sagitova.ma@phystech.edu>
2020-11-16 09:15:36 +0300

    fix

--- contest2/A.cpp
+++ contest2/A.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <set>
 #include <numeric>
+#include <vector>
 
 struct Point {
   int x, y;
 };
+
+std::istream& operator>> (std::istream& is, Point& p)
+{
+  is >> p.x >> p.y;
+  return is;
+}
 
 struct Vector {
   int x, y;
@@ -15,23 +22,40 @@ struct Vector {
   }
 };
 
+struct Segment{
+  Point start;
+  Point end;
+  Segment() = default;
+  Segment(Point& a, Point& b) : start(a), end(b) {}
+  Vector GetVector(){
+    return Vector(start, end);
+  }
+};
+
+std::istream& operator>> (std::istream& is, Segment& s)
+{
+  is >> s.start >> s.end;
+  return is;
+}
+
+
 int CrossProductSign(Vector a, Vector b) {
   return (a.x * b.y - a.y * b.x < 0) ? -1 : 1;
 }
 
-bool AreCrossing(Point a1, Point a2, Point b1, Point b2) {
-  bool different_parts_a = CrossProductSign(Vector(a1, a2), Vector(a1, b1)) != CrossProductSign(Vector(a1, a2),
-                                                                                                Vector(a1, b2));
-  bool different_parts_b = CrossProductSign(Vector(b1, b2), Vector(b1, a1)) != CrossProductSign(Vector(b1, b2),
-                                                                                                Vector(b1, a2));
+bool AreCrossing(Segment a, Segment b) {
+  bool different_parts_a = CrossProductSign(a.GetVector(), Vector(a.start, b.start)) != CrossProductSign(a.GetVector(),
+                                                                                                Vector(a.start, b.end));
+  bool different_parts_b = CrossProductSign(b.GetVector(), Vector(b.start, a.start)) != CrossProductSign(b.GetVector(),
+                                                                                                Vector(b.start, a.end));
   return different_parts_a && different_parts_b;
 
 }
 
-std::pair<int, int> FindCrossingPlace(Point a, Vector A, Point b, Vector B) {
+std::pair<int, int> FindCrossingPlace(Segment A, Segment B) {
   std::pair<int, int> cross_a;
-  cross_a.first = (a.y - b.y) * B.x - (a.x - b.x) * B.y;
-  cross_a.second = B.y * A.x - B.x * A.y;
+  cross_a.first = (A.start.y - B.start.y) * B.GetVector().x - (A.start.x - B.start.x) * B.GetVector().y;
+  cross_a.second = B.GetVector().y * A.GetVector().x - B.GetVector().x * A.GetVector().y;
   int gcd = std::gcd(std::abs(cross_a.first), std::abs(cross_a.second));
   gcd = std::max(1, gcd);
   cross_a.first /= gcd;
@@ -40,15 +64,23 @@ std::pair<int, int> FindCrossingPlace(Point a, Vector A, Point b, Vector B) {
   return cross_a;
 }
 
-int NumOfBridges(Point a, Point b, int num_of_rivers, std::istream& instream) {
+std::vector<Segment> GetRivers(int num_of_rivers, std::istream& instream){
+  Segment river;
+  std::vector<Segment> rivers;
+  for (int i = 0; i < num_of_rivers; ++i) {
+    instream >> river;
+    rivers.push_back(river);
+  }
+  return rivers;
+}
+
+int NumOfBridges(Point a, Point b, std::vector<Segment> rivers) {
   Point river_start, river_end;
   std::set<std::pair<int, int>> bridges;
-  Vector road(a, b);
-  for (int i = 0; i < num_of_rivers; ++i) {
-    instream >> river_start.x >> river_start.y >> river_end.x >> river_end.y;
-    Vector river(river_start, river_end);
-    if (AreCrossing(a, b, river_start, river_end)) {
-      std::pair<int, int> bridge = FindCrossingPlace(a, road, river_start, river);
+  Segment road(a, b);
+  for (auto river:rivers) {
+    if (AreCrossing(road, river)) {
+      std::pair<int, int> bridge = FindCrossingPlace(road, river);
       bridges.insert(bridge);
     }
   }
@@ -58,7 +90,7 @@ int NumOfBridges(Point a, Point b, int num_of_rivers, std::istream& instream) {
 int main() {
   int n;
   Point a, b;
-  std::cin >> a.x >> a.y >> b.x >> b.y;
+  std::cin >> a >> b;
   std::cin >> n;
-  std::cout << NumOfBridges(a, b, n, std::cin);
+  std::cout << NumOfBridges(a, b, GetRivers(n, std::cin));
 }
