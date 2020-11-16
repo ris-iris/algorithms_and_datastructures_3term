026b92aefd9c85e2105552b49bcdbe3d7f57f62c
ris-iris <sagitova.ma@phystech.edu>
2020-11-16 08:55:34 +0300

    fix

--- contest1/F1.cpp
+++ contest1/F1.cpp
@@ -8,39 +8,22 @@
 
 class SuffArray {
  public:
-  SuffArray(std::string s1, int div) : s(std::move(s1)), divider(div) {
-    length_ = s.length();
-    suf_arr_ = std::vector<int>(length_, 0);
-    classes_ = std::vector<int>(length_, 0);
-    cnt_ = std::vector<int>(ALPHABET, 0);
-    lcp_ = std::vector<int>(length_, 0);
+  SuffArray(std::string &s1) : s(s1),
+                               length_(s.length()),
+                               suf_arr_(std::vector<int>(length_, 0)),
+                               classes_(std::vector<int>(length_, 0)),
+                               cnt_( std::vector<int>(ALPHABET, 0)),
+                               lcp_(std::vector<int>(length_, 0)){
     Init();
-    CountSuffArr();
+    count();
     CountLCP();
   }
 
-  std::string GetKStatistic(int64_t k) {
-    int64_t temp_stat = 0;
-    int min_lcp = 0;
-    for (int i = 0; i < length_ - 1; ++i) {
-      if (suf_arr_[i] < divider && suf_arr_[i + 1] > divider || suf_arr_[i] > divider && suf_arr_[i + 1] < divider) {
-        temp_stat += std::max(lcp_[i] - min_lcp, 0);
-
-        if (k <= temp_stat) {
-          int64_t temp_lcp = lcp_[i] - (temp_stat - k);
-          std::string ans = "";
-          for (int j = suf_arr_[i]; j < suf_arr_[i] + temp_lcp; ++j) {
-            ans += s[j];
-          }
-          return ans;
-        } else {
-          min_lcp = lcp_[i];
-        }
-      } else {
-        min_lcp = std::min(lcp_[i], min_lcp);
-      }
-    }
-    return "-1";
+  const std::vector<int> &GetSufArr() const {
+    return suf_arr_;
+  }
+  const std::vector<int> &GetLcp() const {
+    return lcp_;
   }
 
  private:
@@ -50,20 +33,16 @@ class SuffArray {
   std::vector<int> classes_;
   std::vector<int> cnt_;
   std::vector<int> lcp_;
-  int tmpClass = 0;
+  int tmpClass = ALPHABET;
   int divider;
 
   void Init() {
+    std::vector<int> helpSufArr(length_, 0);
     for (int i = 0; i < length_; ++i) {
-      ++cnt_[s[i]];
+      classes_[i] = s[i];
+      helpSufArr[i] = i;
     }
-    for (int i = 1; i < ALPHABET; ++i) {
-      cnt_[i] += cnt_[i - 1];
-    }
-    for (int i = length_ - 1; i > -1; --i) {
-      --cnt_[s[i]];
-      suf_arr_[cnt_[s[i]]] = i;
-    }
+    countSort(helpSufArr);
 
     for (int i = 1; i < length_; ++i) {
       if (s[suf_arr_[i]] != s[suf_arr_[i - 1]]) {
@@ -73,39 +52,43 @@ class SuffArray {
     }
   }
 
-  void CountSuffArr() {
-    std::vector<int> help_suf_arr(length_, 0);
-    std::vector<int> help_classes(length_, 0);
+  void countSort(std::vector<int> &helpSufArr) {
+    for (int i = 0; i < length_; ++i) {
+      ++cnt_[classes_[helpSufArr[i]]];
+    }
+    for (int i = 1; i < tmpClass + 1; ++i) {
+      cnt_[i] += cnt_[i - 1];
+    }
+    for (int i = length_ - 1; i > -1; --i) {
+      --cnt_[classes_[helpSufArr[i]]];
+      suf_arr_[cnt_[classes_[helpSufArr[i]]]] = helpSufArr[i];
+    }
+  }
+
+  std::vector<int> GetNewClasses(std::vector<int> &helpSufArr, int pow) {
+    std::vector<int> helpClasses(length_, 0);
+    countSort(helpSufArr);
+    tmpClass = 0;
+    for (int i = 1; i < length_; ++i) {
+      int secHalf1 = (suf_arr_[i] + (1u << pow)) % length_;
+      int secHalf2 = (suf_arr_[i - 1] + (1u << pow)) % length_;
+      if (classes_[suf_arr_[i]] != classes_[suf_arr_[i - 1]] || classes_[secHalf1] != classes_[secHalf2])
+        ++tmpClass;
+      helpClasses[suf_arr_[i]] = tmpClass;
+    }
+    return helpClasses;
+  }
+
+  void count() {
+    std::vector<int> helpSufArr(length_, 0);
 
     for (uint64_t pow = 0; (1u << pow) < length_; ++pow) {
       cnt_.assign(tmpClass + 1, 0);
-      int mv = (1u << pow);
       for (int i = 0; i < length_; ++i) { // made sorted by second part
-        help_suf_arr[i] = suf_arr_[i] - (1u << pow);
-        if (help_suf_arr[i] < 0) help_suf_arr[i] += length_;
+        helpSufArr[i] = suf_arr_[i] - (1u << pow);
+        if (helpSufArr[i] < 0) helpSufArr[i] += length_;
       }
-      //counting to sort the first part
-      for (int i = 0; i < length_; ++i) {
-        ++cnt_[classes_[help_suf_arr[i]]];
-      }
-      for (int i = 1; i < tmpClass + 1; ++i) {
-        cnt_[i] += cnt_[i - 1];
-      }
-      for (int i = length_ - 1; i > -1; --i) {
-        --cnt_[classes_[help_suf_arr[i]]];
-        suf_arr_[cnt_[classes_[help_suf_arr[i]]]] = help_suf_arr[i];
-      }
-      help_classes.assign(length_, 0);
-      help_classes[suf_arr_[0]] = 0;
-      tmpClass = 0;
-      for (int i = 1; i < length_; ++i) {
-        int secHalf1 = (suf_arr_[i] + (1u << pow)) % length_;
-        int secHalf2 = (suf_arr_[i - 1] + (1u << pow)) % length_;
-        if (classes_[suf_arr_[i]] != classes_[suf_arr_[i - 1]] || classes_[secHalf1] != classes_[secHalf2])
-          ++tmpClass;
-        help_classes[suf_arr_[i]] = tmpClass;
-      }
-      classes_ = help_classes;
+      classes_ = GetNewClasses(helpSufArr, pow);
     }
   }
 
@@ -132,15 +115,46 @@ class SuffArray {
   }
 };
 
+std::string GetKStatistic(std::string &s, std::string &t, int64_t k) {
+  std::string text = s + (char) 0 + t + (char) 1;
+  SuffArray sa(text);
+  int divider = s.length();
+  int length = text.length();
+
+  std::vector<int> suf_arr = sa.GetSufArr();
+  std::vector<int> lcp = sa.GetLcp();
+
+  int64_t temp_stat = 0;
+  int min_lcp = 0;
+  for (int i = 0; i < length - 1; ++i) {
+    if (suf_arr[i] < divider && suf_arr[i + 1] > divider || suf_arr[i] > divider && suf_arr[i + 1] < divider) {
+      temp_stat += std::max(lcp[i] - min_lcp, 0);
+
+      if (k <= temp_stat) {
+        int64_t temp_lcp = lcp[i] - (temp_stat - k);
+        std::string ans = "";
+        for (int j = suf_arr[i]; j < suf_arr[i] + temp_lcp; ++j) {
+          ans += text[j];
+        }
+        return ans;
+      } else {
+        min_lcp = lcp[i];
+      }
+    } else {
+      min_lcp = std::min(lcp[i], min_lcp);
+    }
+  }
+  return "-1";
+}
+
+
 int main() {
   std::string s, t;
   getline(std::cin, s);
   getline(std::cin, t);
   int64_t k;
   std::cin >> k;
-  std::string text = s + (char) 0 + t + (char) 1;
-  SuffArray sa(text, s.length());
-  std::cout << sa.GetKStatistic(k);
+  std::cout << GetKStatistic(s, t, k);
 
   return 0;
 }
