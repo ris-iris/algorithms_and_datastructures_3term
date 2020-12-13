8e75e6a8df80da5760911e0968f3d5d496a565d4
ris-iris <sagitova.ma@phystech.edu>
2020-12-13 12:09:17 +0300

    fix

--- contest2/F1.cpp
+++ contest2/F1.cpp
@@ -17,17 +17,27 @@ enum state {
 };
 
 struct Point {
-  int id;
   long double x, y, z;
-  Point *prev;
-  Point *next;
-
   Point() = default;
-  Point(int _id, long double _x, long double _y, long double _z, Point *p, Point *n)
-      : id(_id), x(_x), y(_y), z(_z), prev(p), next(n) {}
-
   Point(long double _x, long double _y)
-      : id(-1), x(_x), y(_y), z(0), prev(nullptr), next(nullptr) {}
+      : x(_x), y(_y), z(_x * _x + _y * _y) {}
+};
+
+std::istream &operator>>(std::istream &is, Point &p) {
+  is >> p.x >> p.y;
+  p.z = p.x * p.x + p.y * p.y;
+  return is;
+}
+
+struct Node {
+  int id;
+  Point point;
+  Node *prev;
+  Node *next;
+
+  Node() = default;
+  Node(int _id, Point ph, Node *p, Node *n)
+      : id(_id), point(ph), prev(p), next(n) {}
 
   bool GetLinksValid() {
     if (prev->next == this) {
@@ -42,23 +52,32 @@ struct Point {
   }
 };
 
-bool IsRightTurn(const Point *a, const Point *b, const Point *c) {
+long double CrossProduct(Point a, Point b, Point c, char mode) {
+  if (mode == 'y') {
+    return (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x);
+  }
+  if (mode == 'z') {
+    return ((b.x - a.x) * (c.z - b.z) - (b.z - a.z) * (c.x - b.x));
+  }
+}
+
+bool IsRightTurn(const Node *a, const Node *b, const Node *c) {
   if (a == nullptr || b == nullptr || c == nullptr) {
     return false;
   }
-  return (b->x - a->x) * (c->y - b->y) - (b->y - a->y) * (c->x - b->x) < 0;
+  return CrossProduct(a->point, b->point, c->point, 'y') < 0;
 }
 
-long double TurnTime(Point *a, Point *b, Point *c) {
+long double TurnTime(Node *a, Node *b, Node *c) {
   if (a == nullptr || b == nullptr || c == nullptr) {
     return INFTY;
   }
-  long double help = (b->x - a->x) * (c->y - b->y) - (b->y - a->y) * (c->x - b->x);
-  return help == 0 ? INFTY : ((b->x - a->x) * (c->z - b->z) - (b->z - a->z) * (c->x - b->x)) / help;
+  long double help = CrossProduct(a->point, b->point, c->point, 'y');
+  return help == 0 ? INFTY : CrossProduct(a->point, b->point, c->point, 'z') / help;
 }
 
-std::pair<Point *, Point *> FindBridge(Point *first, Point *second) {
-  std::pair<Point *, Point *> bridge(first, second);
+std::pair<Node *, Node *> FindBridge(Node *first, Node *second) {
+  std::pair<Node *, Node *> bridge(first, second);
   while (true) {
     if (IsRightTurn(bridge.first, bridge.second, bridge.second->next)) {
       bridge.second = bridge.second->next;
@@ -71,17 +90,17 @@ std::pair<Point *, Point *> FindBridge(Point *first, Point *second) {
   return bridge;
 }
 
-void FixLinks(std::pair<Point *, Point *> &bridge, std::vector<Point *> &res, long double divider_x) {
+void FixLinks(std::pair<Node *, Node *> &bridge, std::vector<Node *> &res, long double divider_x) {
   bridge.first->next = bridge.second;
   bridge.second->prev = bridge.first;
   for (int i = res.size() - 1; i > -1; --i) {
-    Point *current = res[i];
-    if (current->x > bridge.first->x && current->x < bridge.second->x) {
+    Node *current = res[i];
+    if (current->point.x > bridge.first->point.x && current->point.x < bridge.second->point.x) {
       bridge.first->next = current;
       bridge.second->prev = current;
       current->prev = bridge.first;
       current->next = bridge.second;
-      if (current->x <= divider_x) {
+      if (current->point.x <= divider_x) {
         bridge.first = current;
       } else {
         bridge.second = current;
@@ -98,12 +117,12 @@ void FixLinks(std::pair<Point *, Point *> &bridge, std::vector<Point *> &res, lo
   }
 }
 
-std::vector<Point *> MergeHulls(const std::vector<Point *> &first,
-                                const std::vector<Point *> &second,
-                                Point *first_bridge,
-                                Point *second_bridge) {
-  std::vector<Point *> res;
-  std::pair<Point *, Point *> bridge = move(FindBridge(first_bridge, second_bridge));
+std::vector<Node *> MergeHulls(const std::vector<Node *> &first,
+                               const std::vector<Node *> &second,
+                               Node *first_bridge,
+                               Node *second_bridge) {
+  std::vector<Node *> res;
+  std::pair<Node *, Node *> bridge = move(FindBridge(first_bridge, second_bridge));
 
   int iter1 = 0, iter2 = 0;
   long double current_time = -INFTY;
@@ -147,13 +166,13 @@ std::vector<Point *> MergeHulls(const std::vector<Point *> &first,
     current_time = min_time;
     switch (min_state) {
       case FIRST_PART:
-        if (first[iter1]->x < bridge.first->x)
+        if (first[iter1]->point.x < bridge.first->point.x)
           res.push_back(first[iter1]);
         first[iter1]->GetLinksValid();
         ++iter1;
         break;
       case SECOND_PART:
-        if (second[iter2]->x > bridge.second->x)
+        if (second[iter2]->point.x > bridge.second->point.x)
           res.push_back(second[iter2]);
         second[iter2]->GetLinksValid();
         ++iter2;
@@ -172,16 +191,16 @@ std::vector<Point *> MergeHulls(const std::vector<Point *> &first,
         break;
     }
   }
-  FixLinks(bridge, res, first_bridge->x);
+  FixLinks(bridge, res, first_bridge->point.x);
   return res;
 }
 
-std::vector<Point *> BuildHull(std::vector<Point *> &points, int left, int right) {
-  if (right - left == 1) return std::vector<Point *>({points[left]});
+std::vector<Node *> BuildHull(std::vector<Node *> &points, int left, int right) {
+  if (right - left == 1) return std::vector<Node *>({points[left]});
   int m = (left + right) / 2;
-  std::vector<Point *> help1 = move(BuildHull(points, left, m));
-  std::vector<Point *> help2 = move(BuildHull(points, m, right));
-  std::vector<Point *> res = move(MergeHulls(help1, help2, points[m - 1], points[m]));
+  std::vector<Node *> help1 = move(BuildHull(points, left, m));
+  std::vector<Node *> help2 = move(BuildHull(points, m, right));
+  std::vector<Node *> res = move(MergeHulls(help1, help2, points[m - 1], points[m]));
   return res;
 }
 
@@ -194,19 +213,19 @@ struct Face {
       : first_vertex(f), second_vertex(s), third_vertex(t) {}
 };
 
-void AddLowerHull(std::vector<Point *> &points, std::vector<Face> &result) {
-  std::sort(points.begin(), points.end(), [](Point *a, Point *b) {
-    return a->x < b->x;
+void AddLowerHull(std::vector<Node *> &points, std::vector<Face> &result) {
+  std::sort(points.begin(), points.end(), [](Node *a, Node *b) {
+    return a->point.x < b->point.x;
   });
-  std::vector<Point *> point_seq = move(BuildHull(points, 0, points.size()));
-  for (Point *p : point_seq) {
+  std::vector<Node *> point_seq = move(BuildHull(points, 0, points.size()));
+  for (Node *p : point_seq) {
     Face current(p->prev->id, p->id, p->next->id);
     p->GetLinksValid();
     result.push_back(current);
   }
 }
 
-std::vector<Face> GetFaces(std::vector<Point *> points) {
+std::vector<Face> GetFaces(std::vector<Node *> points) {
   std::vector<Face> result;
   AddLowerHull(points, result);
 
@@ -217,22 +236,25 @@ std::pair<int, int> makeEdge(int a, int b) {
   return std::make_pair(std::min(a, b), std::max(a, b));
 }
 
-long double MeanVoronoyVertex(std::vector<Point *> &points) {
-  Point small = Point(-1, -20000000, 20000000, 2 * 400000000000000, nullptr, nullptr);
-  Point big = Point(-2, 20000000, -20000000, 2 * 400000000000000, nullptr, nullptr);
+long double MeanVoronoyVertex(std::vector<Node *> &points) {
+  long double inf = 20000000;
+  Point inf1(-inf, inf);
+  Point inf2(inf, -inf);
+  Node small = Node(-1, inf1, nullptr, nullptr);
+  Node big = Node(-2, inf2, nullptr, nullptr);
   points.push_back(&small);
   points.push_back(&big);
   std::vector<Face> faces = GetFaces(points);
   std::set<int> extreme_points;
   for (auto f : faces) {
-    if (f.first_vertex < 0 || f.second_vertex < 0 || f.third_vertex < 0){
+    if (f.first_vertex < 0 || f.second_vertex < 0 || f.third_vertex < 0) {
       extreme_points.insert(f.first_vertex);
       extreme_points.insert(f.second_vertex);
       extreme_points.insert(f.third_vertex);
     }
   }
   std::vector<bool> is_extreme(points.size(), false);
-  for (auto e : extreme_points){
+  for (auto e : extreme_points) {
     if (e >= 0) is_extreme[e] = true;
   }
 
@@ -240,13 +262,13 @@ long double MeanVoronoyVertex(std::vector<Point *> &points) {
 
   long double polygon_edges = 0;
   for (auto f : faces) {
-    if (f.first_vertex >= 0 && !is_extreme[f.first_vertex]){
+    if (f.first_vertex >= 0 && !is_extreme[f.first_vertex]) {
       ++polygon_edges;
     }
-    if (f.second_vertex >= 0 && !is_extreme[f.second_vertex]){
+    if (f.second_vertex >= 0 && !is_extreme[f.second_vertex]) {
       ++polygon_edges;
     }
-    if (f.third_vertex >= 0 && !is_extreme[f.third_vertex]){
+    if (f.third_vertex >= 0 && !is_extreme[f.third_vertex]) {
       ++polygon_edges;
     }
   }
@@ -257,10 +279,11 @@ long double MeanVoronoyVertex(std::vector<Point *> &points) {
 
 int main() {
   long double x, y;
-  std::vector<Point *> points;
+  std::vector<Node *> points;
   int n = 0;
-  while ((std::cin >> x >> y)) {
-    Point *a = new Point(n, x, y, x * x + y * y, nullptr, nullptr);
+  Point p{};
+  while ((std::cin >> p)) {
+    Node *a = new Node(n, p, nullptr, nullptr);
     points.push_back(a);
     ++n;
   }
